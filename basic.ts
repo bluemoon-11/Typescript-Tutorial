c62c1958f726510014c2003c444e1cbdc34570db
bluemoon-11 <244584119+bluemoon-11@users.noreply.github.com>
2026-01-19 11:32:27 +0530

    add basic greeting functions and console logs

---
+++ basic.ts
@@ -0,0 +1,10 @@
+const greet = (name : string) : string=>{
+    return `${name}`
+}
+
+function greet2 (name : string) : string {
+    return `${name}`
+}
+
+console.log(greet("Avranil"));
+console.log(greet2("Avranil"));
